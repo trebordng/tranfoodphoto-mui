de1a2fa7d30f6c8e561f29cc450c224a28f93f3c
trebordng <96870545+trebordng@users.noreply.github.com>
2022-07-07 15:33:00 +1000

    change legacy

--- components/BookLayout.js
+++ components/BookLayout.js
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React, { useEffect } from "react";
 import Image from "next/image";
 import { setGlobalState, useGlobalState } from "../state/index";
-import Fade from "react-reveal/Fade";
+import { Fade } from "react-awesome-reveal";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const BookLayout = ({ children }) => {
@@ -10,7 +10,7 @@ const BookLayout = ({ children }) => {
 
   return isLoading === true ? (
     <div className="loading-bg">
-      <CircularProgress className="loading"/>
+      <CircularProgress className="loading" />
     </div>
   ) : (
     <Fade right>
