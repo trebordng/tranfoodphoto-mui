29bf9fd01ceb5f2c75644b91c5c6ebbb797013d9
trebordng <96870545+trebordng@users.noreply.github.com>
2022-07-09 09:34:05 +1000

    home page

--- state/index.js
+++ state/index.js
@@ -3,7 +3,7 @@ import { createGlobalState } from "react-hooks-global-state";
 const {setGlobalState,useGlobalState} = createGlobalState({
     mode:'light',
     loading:true,
-    cover:""
+    cover:undefined,
 })
 
 export {setGlobalState,useGlobalState};
