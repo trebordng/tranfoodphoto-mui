b0c26627ffe5411d60a85c7c94296197c2771576
trebordng <96870545+trebordng@users.noreply.github.com>
2022-07-10 00:29:00 +1000

    home page

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,17 @@ import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
 import React, { useState } from "react";
 import BookLayout from "../components/BookLayout";
-import Image from "next/image";
-import homeImg from "../public/page-image/landing-image.jpg";
 import { useRouter } from "next/router";
 import en from "../locales/en";
 import vie from "../locales/vie";
 const Index = ({ images, checkImages }) => {
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { locale } = router;
   const t = locale === "en" ? en : vie;
 
   return (
     <BookLayout>
-      <Image
-     
-        src={homeImg.src}
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        className="bg-image"
-      />
+      <Typography className="bg-image"></Typography>
     
         <Box className="book-title">
           <Typography variant="h5">{t.pageTitle}</Typography>
